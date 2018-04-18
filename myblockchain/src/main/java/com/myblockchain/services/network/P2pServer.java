57e719e60ee545ca91ca7153cfd610bc029fc7c1
Kevin Ming <kevin93.ming@gmail.com>
2018-04-18 17:51:06 -0400

    start to build p2p network

---
+++ myblockchain/src/main/java/com/myblockchain/services/network/P2pServer.java
@@ -0,0 +1,13 @@
+package com.myblockchain.services.network;
+
+import java.net.ServerSocket;
+
+public class P2pServer implements Runnable {
+
+    private ServerSocket serverSocket;
+
+    public void run() {
+
+    }
+
+}
